322ef124769d4b16d3edf6c7f09805814a120726
Bram Ver <bramvereertbrugghen@outlook.com>
2018-06-03 13:07:17 +0200

    Changed mouse speed settings

--- xd75re/rev01/config.h
+++ xd75re/rev01/config.h
@@ -19,10 +19,10 @@
 
 #include "../../config.h"
 
-#define MOUSEKEY_DELAY             300
-#define MOUSEKEY_INTERVAL          50
-#define MOUSEKEY_MAX_SPEED         20
-#define MOUSEKEY_TIME_TO_MAX       20
+#define MOUSEKEY_DELAY             200
+#define MOUSEKEY_INTERVAL          40
+#define MOUSEKEY_MAX_SPEED         10
+#define MOUSEKEY_TIME_TO_MAX       40
 #define MOUSEKEY_WHEEL_MAX_SPEED   8
 #define MOUSEKEY_WHEEL_TIME_TO_MAX 40
 
