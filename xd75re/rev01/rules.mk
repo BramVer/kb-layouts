b955a2ca7065b2ac8ed2cc3058f689b855fd5301
Bram Ver <bramvereertbrugghen@outlook.com>
2018-06-03 12:41:52 +0200

    Updated make files with latest layout

--- xd75re/rev01/rules.mk
+++ xd75re/rev01/rules.mk
@@ -1,10 +1,9 @@
 # Build options
 BACKLIGHT_ENABLE = yes		# Switch LEDs
 MOUSEKEY_ENABLE = yes		# Emulates mouse key using keypresses
-RGBLIGHT_ENABLE = no 		# LED strips
+RGBLIGHT_ENABLE = yes 		# LED strips
 TAP_DANCE_ENABLE = no 		# Use multi-tap features
 AUDIO_ENABLE = no  			# Audio stuff
-RGBLIGHT_ENABLE = yes
 
 ifndef QUANTUM_DIR
 	include ../../../../Makefile
